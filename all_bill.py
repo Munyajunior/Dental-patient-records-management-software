a6e5d53b3b555aa3c3ac05a88e5a229f2830629b
Munyajunior <munyajunior@icloud.com>
2024-04-24 16:17:20 +0100

    recoded layout display

--- all_bill.py
+++ all_bill.py
@@ -46,29 +46,37 @@ class V_billClass(ctk.CTk):
         
         
         #========================Bill List=======================
-        vbill_list_frame=ctk.CTkFrame(self.root,width=screen_width+100,height=600)
+        vbill_list_frame=ctk.CTkFrame(self.root)
         vbill_list_frame.place(x=50,y=140)
         
+        
+        self.v_bill_list=Listbox(vbill_list_frame,font=("goudy old style",15),bg="white",width=30,height=20)
         scrolly=Scrollbar(vbill_list_frame,orient=VERTICAL)
-        self.v_bill_list=Listbox(vbill_list_frame,font=("goudy old style",15),bg="white",yscrollcommand=scrolly.set)
+        scrollx=Scrollbar(vbill_list_frame,orient=HORIZONTAL)
         scrolly.pack(side=RIGHT,fill=Y)
+        scrollx.pack(side=BOTTOM,fill=X)
+        self.v_bill_list.configure(yscrollcommand=scrolly.set,xscrollcommand=scrollx.set)
         scrolly.configure(command=self.v_bill_list.yview)
+        scrollx.configure(command=self.v_bill_list.xview)
         self.v_bill_list.pack(fill=BOTH,expand=1)
         self.v_bill_list.bind("<ButtonRelease-1>",self.get_data)
         
         #===============BILL AREA==============
-        bill_x_position=screen_width-1050
-        bill_y_position=screen_height-630
+        
         vbill_frame=ctk.CTkFrame(self.root)
-        vbill_frame.place(x=bill_x_position,y=140)
+        vbill_frame.place(x=400,y=140)
         
-        lbl_title2=Label(vbill_frame,text="Patient Bill Area",font=("goudy old style",20),width=int(screen_width/2-700+50)).pack(side=TOP,fill=X)
-        
-        
+        lbl_title2=Label(vbill_frame,text="Patient Bill Area",font=("goudy old style",20),width=20).pack(side=TOP,fill=X)
         scrolly2=Scrollbar(vbill_frame,orient=VERTICAL)
-        self.bill_area=ctk.CTkTextbox(vbill_frame,bg_color="lightyellow",yscrollcommand=scrolly2.set)
+        scrollx2=Scrollbar(vbill_frame,orient=HORIZONTAL)
         scrolly2.pack(side=RIGHT,fill=Y)
-        scrolly2.config(command=self.bill_area.yview)
+        scrollx2.pack(side=BOTTOM,fill=X)
+        
+        
+        self.bill_area=ctk.CTkTextbox(vbill_frame,bg_color="lightyellow",width=430,height=420)
+        self.bill_area.configure(yscrollcommand=scrolly2.set,xscrollcommand=scrollx2.set)
+        scrollx2.configure(command=self.bill_area.xview)
+        scrolly2.configure(command=self.bill_area.yview)
         self.bill_area.pack(fill=BOTH,expand=1)
         
         #====================Images===============
@@ -76,7 +84,7 @@ class V_billClass(ctk.CTk):
                                      light_image=Image.open(resource_path("images\\bill.jpeg")),
                                      size=(300,250))
         lbl_image=ctk.CTkLabel(self.root,image=self.bill_photo)
-        lbl_image.place(x=screen_width-500,y=140)
+        lbl_image.place(x=900,y=140)
         
         #==================Footer================
         date_=time.strftime("%Y")
